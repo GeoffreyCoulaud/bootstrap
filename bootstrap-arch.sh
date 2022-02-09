68c617e9ed5853fbb9a181f46154c73c6f5cfec1
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2022-02-09 21:25:58 +0100

    Updated boostrap script (flatpak support)

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-# Update mirrors
-sudo pacman-mirrors --geoip && sudo pacman -Syyu
+echo "#########################################################################"
+echo "# Updating pacman mirrors"
+echo "#########################################################################"
+sudo pacman-mirrors --geoip && sudo pacman -Syyu --noconfirm
 
-# Update system and add apps
-cat ./bootstrap-arch-repos.txt | sudo pacman -Syu -
-cat ./bootstrap-arch-aur.txt | yay -S -
+echo "#########################################################################"
+echo "# Installing from the official repos"
+echo "#########################################################################"
+cat ./bootstrap-arch-repos.txt | sudo pacman -Syu --noconfirm -
+
+echo "#########################################################################"
+echo "# Installing from the AUR"
+echo "#########################################################################"
+cat ./bootstrap-arch-aur.txt | yay -S --norebuild --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --removemake --batchinstall -
+
+echo "#########################################################################"
+echo "# Installing from flathub"
+echo "#########################################################################"
+while IFS= read -r app; do
+	flatpak install -y flathub "$app"
+done < "./bootstrap-flathub.txt"
+flatpak update -y
